dd24eb18d42857d8abe49013ceb7d085154141ef
JengYoung <young_pallete@naver.com>
2022-09-28 19:52:51 +0900

    :construction_worker: aws s3 코드 추가
    다음을 참고하였다. https://github.com/aws-actions/configure-aws-credentials#assuming-a-role

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,7 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-        <ExampleComponentCSS>TEST</ExampleComponentCSS>
+        <ExampleComponentCSS>TEST!!!</ExampleComponentCSS>
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.tsx</code>
