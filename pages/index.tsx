4b4530acb8b18b57ffd2beb2a40aef24b5cd0b44
JengYoung <young_pallete@naver.com>
2022-09-29 15:09:10 +0900

    :construction_worker: 린트 검사 워크플로우 추가
    ESLint와 Stylelint를 검사하도록 설정

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import styled from '@emotion/styled';
+import { css } from '@emotion/react';
 
 const ExampleComponentCSS = styled.div`
   width: 1rem;
