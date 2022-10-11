efc8a0af1b2f24e4c1c55a2e14db1d8218abf7f1
JengYoung <young_pallete@naver.com>
2022-10-12 00:50:29 +0900

    :sparkles: 페이지에 들어갈 문구 추가
    뭔가 더 쓰고 싶기는 한데, 구구절절이 될까봐 다 지우고 알짜배기만 써야겠다. 😭 (추가로 나중에 여유가 생기면 손을 흔드는 애니메이션도 만들어야겠다.)

--- components/Text/TransitionText.tsx
+++ components/Text/TransitionText.tsx
@@ -1,18 +1,38 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import React, { ReactNode, useRef } from 'react';
 import useIntersectionObserver from '../../hooks/useIntersectionObserver';
 
-const Text = styled.div`
+interface TextInterface {
+  $delay: number;
+  $size: string;
+}
+
+interface TransitionTextInterface extends TextInterface {
+  children: ReactNode;
+}
+
+const sizes = {
+  xs: '0.5rem',
+  sm: '0.75rem',
+  md: '1rem',
+  lg: '1.5rem',
+  xl: '2rem',
+} as const;
+
+const Text = styled.div<TextInterface>`
   font-family: 'Gowun Batang', serif;
   font-size: 2rem;
   font-weight: 400;
 
-  color: white;
   opacity: 0;
 
-  transition: all 1s;
-  transform: translateY(1rem);
+  ${({ $delay, $size }) => css`
+    font-size: ${sizes[$size as keyof typeof sizes]};
+    transition: all ${$delay}s;
+  `}
 
+  transform: translateY(1rem);
   &.text--active {
     opacity: 1;
     transform: translateY(0);
@@ -23,10 +43,11 @@ const Text = styled.div`
   }
 `;
 
-interface TransitionTextInterface {
-  children: ReactNode;
-}
-const TransitionText = ({ children }: TransitionTextInterface) => {
+const TransitionText = ({
+  $delay,
+  $size,
+  children,
+}: TransitionTextInterface) => {
   const textRef = useRef<HTMLDivElement | null>(null);
   const callbackRef = useRef(() => {
     textRef.current && textRef.current.classList.add('text--active');
@@ -34,7 +55,11 @@ const TransitionText = ({ children }: TransitionTextInterface) => {
 
   useIntersectionObserver(textRef, callbackRef, {});
 
-  return <div>{children}</div>;
+  return (
+    <Text $delay={$delay} $size={$size} ref={textRef}>
+      {children}
+    </Text>
+  );
 };
 
 export default TransitionText;
