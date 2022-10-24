40cc789339af6dfaf290dd448ccd0792f4daaa37
JengYoung <young_pallete@naver.com>
2022-10-24 18:54:13 +0900

    :recycle: 메인카피, 서브카피 스타일 적용 용이하도록 리팩토링, 소개하는 문장 작성
    Text라는 컴포넌트 안에서, 스타일을 객체로 묶어서 사용한다.
    SubCopy의 경우에는, 확장할 일이 생길 것 같아 스타일 사양은 따로 정하지 않았으나 변수로 캐싱하였다.

---
+++ components/Text/index.ts
@@ -0,0 +1,76 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
+const commonStyle = css`
+  margin: 0;
+  padding: 0;
+  color: #fff;
+`;
+
+const XSmall = styled.h5`
+  font-size: 1.25rem;
+  font-weight: 400;
+
+  ${commonStyle}
+`;
+
+const Small = styled.h4`
+  font-size: 1.25rem;
+  font-weight: 400;
+
+  ${commonStyle}
+`;
+
+const Default = styled.div`
+  font-size: 1.5rem;
+  font-weight: 400;
+  line-height: 1.75;
+
+  ${commonStyle}
+`;
+
+const Large = styled.h3`
+  font-size: 1.75rem;
+  font-weight: 700;
+  line-height: 1.75;
+
+  ${commonStyle}
+`;
+
+const XLarge = styled.h2`
+  font-size: 2rem;
+  font-weight: 700;
+  line-height: 2;
+
+  ${commonStyle}
+`;
+
+const XXLarge = styled.h1`
+  font-size: 3rem;
+  font-weight: 900;
+
+  ${commonStyle}
+`;
+
+const MainCopy = styled(XXLarge)`
+  margin-bottom: 1rem;
+`;
+
+/**
+ * @descriptions
+ * TODO: 아직은 구체적인 사양을 정하지 않았으나 추후 확장하기 용이하도록 변수로 할당하였다.
+ */
+const SubCopy = styled(Default)``;
+
+const CopyStyle = {
+  XSmall,
+  Small,
+  Default,
+  Large,
+  XLarge,
+  XXLarge,
+  MainCopy,
+  SubCopy,
+};
+
+export default CopyStyle;
--- pages/about/Introduction.tsx
+++ pages/about/Introduction.tsx
@@ -1,3 +1,4 @@
+import TextStyle from '@components/Text';
 import TransitionText from '@components/Text/TransitionText';
 import styled from '@emotion/styled';
 import React from 'react';
@@ -7,11 +8,26 @@ const StyledIntroduction = styled.section`
   opacity: 1;
 `;
 
+const SubCopy = styled.div`
+  font-size: 1rem;
+`;
+
 const Introduction = () => {
   return (
     <StyledIntroduction>
       <TransitionText $delay={1} $size="xxl" $fontWeight="extrabold">
-        <strong>저</strong>는 말이죠 👀
+        <TextStyle.MainCopy>
+          <div>좋은 코드에는</div>
+          <strong>이야기</strong>가 있다고 믿어요 🙆🏻
+        </TextStyle.MainCopy>
+
+        <TextStyle.Default>
+          <div>한 줄의 빈약한 문장이 작품의 집중을 방해하듯</div>
+          <div>사소한 UX라도 개선하며</div>
+          <div>주석 하나라도 애정을 담아 작성합니다.</div>
+          <div>유저도, 개발자도</div>
+          <div>이해할 수 있는 이야기를 만들고자 노력합니다.</div>
+        </TextStyle.Default>
       </TransitionText>
     </StyledIntroduction>
   );
