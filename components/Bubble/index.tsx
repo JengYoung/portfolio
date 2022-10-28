19ef57de4fadf3102dc35e4e7f053074b6a47e1d
JengYoung <young_pallete@naver.com>
2022-10-28 18:51:42 +0900

    :lipstick: bubble을 click할 시 다른 관련 기술들이 active되도록 구현
    아직 디테일한 색상은 정하지 않았다. 추후 모든 것을 정리할 때 한 번에 할 예정이다.

--- components/Bubble/index.tsx
+++ components/Bubble/index.tsx
@@ -1,36 +1,79 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import React, { MouseEventHandler } from 'react';
 
 interface BubbleProps {
   children: React.ReactNode;
+  className: string;
   onClick: MouseEventHandler;
 }
 
-const StyledBubbleContainer = styled.div`
-  position: inline-block;
-  display: center;
-  align-items: center;
-  justify-content: center;
+enum BgColors {
+  Default = '#b791f3',
+  Active = '#ff04b4',
+}
+
+const StyledPseudoElement = (bg: string) => css`
+  position: absolute;
+
+  display: block;
   width: 100%;
   height: 100%;
+
+  content: '';
+
+  background: radial-gradient(transparent, 70%, ${bg});
+
+  border-radius: 50%;
+
+  transition: all 1s;
+`;
+
+const StyledBubbleContainer = styled.div`
+  position: relative;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 100%;
+
   cursor: pointer;
-  background: radial-gradient(transparent, 70%, #b791f3);
 
   border-radius: 50%;
 
   transition: all 0.5s ease-in;
 
-  &:hover {
+  &:before {
+    ${StyledPseudoElement(BgColors.Default)}
     opacity: 1;
-    transition: all 1s;
+  }
+  &::after {
+    ${StyledPseudoElement(BgColors.Active)}
+    opacity: 0;
+  }
 
+  &:hover {
     transform: scale(1.05);
+  }
+
+  &.bubble--active {
+    &:before {
+      opacity: 0;
+    }
+
+    &:after {
+      opacity: 1;
+    }
   }
 `;
 
-const Bubble = ({ children, onClick }: BubbleProps) => {
+const Bubble = ({ className, children, onClick }: BubbleProps) => {
   return (
-    <StyledBubbleContainer onClick={onClick}>{children}</StyledBubbleContainer>
+    <StyledBubbleContainer className={className} onClick={onClick}>
+      {children}
+    </StyledBubbleContainer>
   );
 };
 
--- pages/skills/index.tsx
+++ pages/skills/index.tsx
@@ -8,7 +8,25 @@ interface SkillInterface {
   name: string;
   src: string;
   checks: string[];
+  relativeSkills: string[];
 }
+
+const Skills = {
+  HTML5: 'HTML5',
+  CSS3: 'CSS3',
+  JavaScript: 'JavaScript',
+  TypeScript: 'TypeScript',
+  React: 'React',
+  Vue3: 'Vue3',
+  NextJS: 'NextJS',
+  CSSInJS: 'CSS in JS',
+  Storybook: 'Storybook',
+  Quasar: 'Quasar',
+  NodeJS: 'Node.js',
+  AWS: 'AWS',
+  YarnBerry: 'Yarn berry',
+  CICD: 'CICD',
+} as const;
 
 const SkillsPage = () => {
   const [activeSkill, setActiveSkill] = useState<SkillInterface | null>(null);
@@ -17,18 +35,19 @@ const SkillsPage = () => {
     setActiveSkill(() => skill);
   };
 
-  const skills = [
+  const skills: SkillInterface[] = [
     {
-      name: 'HTML5',
+      name: Skills.HTML5,
       src: '/html.png',
       checks: [
         '주요 태그들이 의미하는 특성을 이해하고 있어요.',
         '효율적으로 구조를 짜고, 시멘틱하게 작성할 수 있어요.',
         'HTML의 파싱과정을 이해하고 있어요.',
       ],
+      relativeSkills: [Skills.JavaScript, Skills.CSS3],
     },
     {
-      name: 'CSS3',
+      name: Skills.CSS3,
       src: '/css3.png',
       checks: [
         'SCSS 구문을 사용할 수 있어요.',
@@ -37,27 +56,40 @@ const SkillsPage = () => {
         'flex와 grid를 자유자재로 사용할 수 있어요.',
         '미디어 쿼리를 사용할 수 있어요.',
       ],
+      relativeSkills: [Skills.JavaScript, Skills.HTML5, Skills.CSSInJS],
     },
     {
-      name: 'JavaScript',
+      name: Skills.JavaScript,
       src: '/javascript.png',
       checks: [
         'ES13까지 틈틈이 최근 ECMAScript의 동향을 파악하고 있어요.',
         'this, 클로저, 실행 컨텍스트 등의 전반적인 자바스크립트 동작을 이해해요.',
         'Canvas API를 다룰 수 있으며 JS에서 애니메이션을 핸들링할 수 있어요.',
       ],
+      relativeSkills: [
+        Skills.TypeScript,
+        Skills.CSS3,
+        Skills.HTML5,
+        Skills.React,
+        Skills.Vue3,
+        Skills.CSSInJS,
+        Skills.NextJS,
+        Skills.Quasar,
+        Skills.NodeJS,
+      ],
     },
     {
-      name: 'TypeScript',
+      name: Skills.TypeScript,
       src: '/typescript.png',
       checks: [
         '프로젝트에서 타입스크립트를 다루는 데 어려움이 없어요.',
         '중복되는 인터페이스 구조 생성을 지양하고 타입의 재사용을 지향해요.',
         'any를 지양하며, type assertion을 남발하지 않아요.',
       ],
+      relativeSkills: [Skills.JavaScript],
     },
     {
-      name: 'React',
+      name: Skills.React,
       src: '/react.png',
       checks: [
         '라이프사이클과 재조정 과정을 이해하고 있어요',
@@ -67,9 +99,10 @@ const SkillsPage = () => {
         'Redux를 사용할 수 있어요',
         '컴포넌트의 재사용을 지향하며 코드를 작성해요.',
       ],
+      relativeSkills: [Skills.Vue3, Skills.NextJS, Skills.CSSInJS],
     },
     {
-      name: 'Vue3',
+      name: Skills.Vue3,
       src: '/vue3.png',
       checks: [
         'Vue의 라이프사이클을 이해하고 있어요.',
@@ -79,73 +112,82 @@ const SkillsPage = () => {
         'Vuex와 Pinia를 다룰 수 있어요.',
         '컴포넌트의 재사용을 지향하며 코드를 작성해요.',
       ],
+      relativeSkills: [Skills.Quasar, Skills.React],
     },
     {
-      name: 'NextJS',
+      name: Skills.NextJS,
       src: '/nextjs.png',
       checks: [
         'CSR, SSR, SSG, ISR의 차이점을 이해하고 있어요.',
         'NextJS가 주는 여러 최적화를 다룰 수 있어요.',
       ],
+      relativeSkills: [Skills.React],
     },
     {
-      name: 'CSS in JS',
+      name: Skills.CSSInJS,
       src: '/emotion.png',
       checks: [
         '@emotion과 Styled-components를 사용할 수 있어요.',
         '무분별한 스타일 컴포넌트 생산이 아닌 합성을 통한 재사용을 지향해요.',
       ],
+      relativeSkills: [Skills.React],
     },
     {
-      name: 'Storybook',
+      name: Skills.Storybook,
       src: '/storybook.png',
       checks: [
         'React와 Vue에서 모두 처음부터 구성하고 사용할 수 있어요.',
         'Control을 이용하여 테스트할 수 있는 스토리북을 만들어요.',
       ],
+      relativeSkills: [Skills.React, Skills.Vue3],
     },
     {
-      name: 'Quasar',
+      name: Skills.Quasar,
       src: '/quasar.svg',
       checks: [
         'Quasar가 주는 컴포넌트, CSS 기능들을 이해하고 있어요',
         'Quasar 기반 반응형 웹을 구축한 경험을 갖고 있어요.',
         'Icongenie 등을 다룰 수 있으며, 하이브리드 앱 유지 보수 경험이 있어요.',
       ],
+      relativeSkills: [Skills.Vue3],
     },
     {
-      name: 'Node.js',
+      name: Skills.NodeJS,
       src: '/nodejs.png',
       checks: [
         'Express.js와 Koa.js를 사용해본 경험이 있어요',
         'Node.js의 이벤트 루프를 이해하고 있어요.',
         'REST API로 클라이언트와 통신하는 서버를 만들 수 있어요.',
       ],
+      relativeSkills: [Skills.JavaScript],
     },
     {
-      name: 'AWS',
+      name: Skills.AWS,
       src: '/aws.png',
       checks: [
         'S3를 이용하여 스토리지 사용 및 정적 웹사이트를 호스팅할 수 있어요.',
         'CloudFront를 다룰 수 있어요.',
       ],
+      relativeSkills: [Skills.CICD],
     },
     {
-      name: 'Yarn berry',
+      name: Skills.YarnBerry,
       src: '/yarn.png',
       checks: [
         'npm이 가진 한계점과 yarn berry의 특성을 이해하고 있어요.',
         '워크스페이스를 나누어 모노레포로 프로젝트를 관리할 수 있어요.',
       ],
+      relativeSkills: [Skills.CICD],
     },
     {
-      name: 'CICD',
+      name: Skills.CICD,
       src: '/github-actions.png',
       checks: [
         '클라이언트 훅과 서버 훅을 이해하고 있어요.',
         'Git hook을 기반으로 husky와 github-action을 다룰 수 있어요.',
         'AWS와 연동하여 배포 자동화를 할 수 있어요.',
       ],
+      relativeSkills: [Skills.AWS, Skills.YarnBerry],
     },
   ];
 
@@ -177,7 +219,17 @@ const SkillsPage = () => {
           {skills.map((skill) => {
             return (
               <Styled.BubbleItem key={skill.name}>
-                <Bubble onClick={() => onClick(skill)}>{skill.name}</Bubble>
+                <Bubble
+                  className={
+                    skill.name === activeSkill?.name ||
+                    activeSkill?.relativeSkills.includes(skill.name)
+                      ? 'bubble--active'
+                      : ''
+                  }
+                  onClick={() => onClick(skill)}
+                >
+                  {skill.name}
+                </Bubble>
               </Styled.BubbleItem>
             );
           })}
