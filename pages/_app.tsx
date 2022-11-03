aa38ba80ec3003f810e268e808aeba9ac090b281
JengYoung <young_pallete@naver.com>
2022-11-03 17:40:45 +0900

    :lipstick: 디자인 시스템에 따른 폰트를 정의한다

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
+import { ThemeProvider } from '@emotion/react';
 import React, { ReactElement, ReactNode } from 'react';
 
 import { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 
+import globalTheme from '@styles/globalTheme';
 import '@styles/globals.css';
 
 /**
@@ -23,8 +25,10 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
 
   // NOTE: 이는 Next.js에서 기본적으로 주어진 코드이므로 그냥 사용하려 한다.
-  /* eslint-disable-next-line react/jsx-props-no-spreading */
-  return getLayout(<Component {...pageProps} />);
+  return (
+    /* eslint-disable-next-line react/jsx-props-no-spreading */
+    <ThemeProvider theme={globalTheme}>{getLayout(<Component {...pageProps} />)}</ThemeProvider>
+  );
 }
 
 export default MyApp;
--- styles/globalTheme.ts
+++ styles/globalTheme.ts
@@ -1,14 +1,44 @@
-const globalTheme = {
-  colors: {
-    primary: {
-      dark: '#5200FF',
-      light: '#FFD600',
-    },
-    subPrimary: '#333',
-    danger: '#FE0101',
-    warning: '#FF7B01',
-    success: '#61C454',
+const colors = {
+  primary: {
+    dark: '#5200FF',
+    light: '#FFD600',
   },
+  subPrimary: '#333',
+  danger: '#FE0101',
+  warning: '#FF7B01',
+  success: '#61C454',
+};
+
+const fontWeights = {
+  extrabold: 900,
+  bold: 700,
+  default: 400,
+};
+
+const heads = {
+  1: ['64px', fontWeights.extrabold],
+  2: ['48px', fontWeights.bold],
+  3: ['40px', fontWeights.bold],
+  4: ['32px', fontWeights.bold],
+};
+
+const fontSizes = {
+  max: '32px',
+  xxl: '24px',
+  xl: '20px',
+  l: '18px',
+  default: '16px',
+  s: '14px',
+  xs: '12px',
+  xxs: '10px',
+  min: '8px',
+};
+
+const globalTheme = {
+  colors,
+  fontWeights,
+  heads,
+  fontSizes,
 };
 
 export default globalTheme;
