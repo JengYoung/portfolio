e807bfa61e3b7d2f8cdd01cfda2145014196888a
JengYoung <young_pallete@naver.com>
2022-11-17 14:51:03 +0900

    :recycle: 컴포지트 패턴을 좀 더 가독성 높게 볼 수 있도록 리팩토링한다

--- components/GitGraph/Main.tsx
+++ components/GitGraph/Main.tsx
@@ -1,39 +1,37 @@
 import React from 'react';
 
-import { StyledGitGraph } from './styles';
+import { Styled } from './styles';
 import { GitGraphInterface } from './types';
 
 function GitGraph({ shouldDraw, shouldShowHistory, nowExperience }: GitGraphInterface) {
   return (
-    <StyledGitGraph.Container>
-      <StyledGitGraph.Branch.Container draw={shouldDraw} shouldShowHistory={shouldShowHistory}>
-        <StyledGitGraph.Branch.MergedCommitContainer>
-          <StyledGitGraph.Branch.MergedBranch />
-          <StyledGitGraph.Branch.MergedCommit>
-            <StyledGitGraph.History.Dot main period={nowExperience.period.end} />
-            <StyledGitGraph.History.CommitMessage main>
-              {nowExperience.title}
-            </StyledGitGraph.History.CommitMessage>
-          </StyledGitGraph.Branch.MergedCommit>
-        </StyledGitGraph.Branch.MergedCommitContainer>
+    <Styled.Container>
+      <Styled.Branch.Container draw={shouldDraw} shouldShowHistory={shouldShowHistory}>
+        <Styled.Branch.MergedCommitContainer>
+          <Styled.Branch.MergedBranch />
+          <Styled.Branch.MergedCommit>
+            <Styled.History.Dot main period={nowExperience.period.end} />
+            <Styled.History.CommitMessage main>{nowExperience.title}</Styled.History.CommitMessage>
+          </Styled.Branch.MergedCommit>
+        </Styled.Branch.MergedCommitContainer>
 
         {nowExperience.contents.map((content) => (
-          <StyledGitGraph.History.Container key={content}>
-            <StyledGitGraph.History.Dot />
-            <StyledGitGraph.History.CommitMessage>{content}</StyledGitGraph.History.CommitMessage>
-            <StyledGitGraph.History.Line />
-          </StyledGitGraph.History.Container>
+          <Styled.History.Container key={content}>
+            <Styled.History.Dot />
+            <Styled.History.CommitMessage>{content}</Styled.History.CommitMessage>
+            <Styled.History.Line />
+          </Styled.History.Container>
         ))}
 
-        <StyledGitGraph.Branch.MergedCommitContainer>
-          <StyledGitGraph.Branch.BasedBranch />
-          <StyledGitGraph.Branch.BasedCommit>
-            <StyledGitGraph.History.Dot main period={nowExperience.period.start} />
-            <StyledGitGraph.History.CommitMessage main />
-          </StyledGitGraph.Branch.BasedCommit>
-        </StyledGitGraph.Branch.MergedCommitContainer>
-      </StyledGitGraph.Branch.Container>
-    </StyledGitGraph.Container>
+        <Styled.Branch.MergedCommitContainer>
+          <Styled.Branch.BasedBranch />
+          <Styled.Branch.BasedCommit>
+            <Styled.History.Dot main period={nowExperience.period.start} />
+            <Styled.History.CommitMessage main />
+          </Styled.Branch.BasedCommit>
+        </Styled.Branch.MergedCommitContainer>
+      </Styled.Branch.Container>
+    </Styled.Container>
   );
 }
 
--- components/GitGraph/styles.ts
+++ components/GitGraph/styles.ts
@@ -1,7 +1,7 @@
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
-const Commit = styled.div`
+const CommonCommit = styled.div`
   position: absolute;
   top: -1.5rem;
   left: -1.5rem;
@@ -10,122 +10,125 @@ const Commit = styled.div`
   color: ${({ theme }) => theme.colors.primary.light};
 `;
 
-const Branch = styled.div`
+const CommonBranch = styled.div`
   position: relative;
   width: 3rem;
   height: 3rem;
   border: 2px solid ${({ theme }) => theme.colors.success};
 `;
 
-export const StyledGitGraph = {
+export const Branch = {
+  Container: styled.div<{ draw: boolean; shouldShowHistory: boolean }>`
+    flex-shrink: 0;
+    width: 100%;
+    height: 100%;
+    opacity: 0;
+    transition: opacity 0.3s;
+
+    ${({ draw, shouldShowHistory }) =>
+      draw &&
+      shouldShowHistory &&
+      css`
+        transform-origin: left;
+        animation: scale-up 1s ease-out forwards;
+        animation-delay: 0.25s;
+        @keyframes scale-up {
+          0% {
+            transform: scale(1);
+          }
+          50% {
+            opacity: 1;
+            transform: scale(1.05);
+          }
+          100% {
+            opacity: 1;
+            transform: scale(1);
+          }
+        }
+      `}
+  `,
+  MergedCommitContainer: styled.div`
+    position: relative;
+    display: flex;
+    align-items: flex-start;
+    width: 100%;
+    height: 3rem;
+  `,
+  MergedBranch: styled(CommonBranch)`
+    border-bottom: 0;
+    border-left: 0;
+    border-top-right-radius: 5rem;
+  `,
+
+  BasedBranch: styled(CommonBranch)`
+    border-top: 0;
+    border-left: 0;
+    border-bottom-right-radius: 5rem;
+  `,
+  MergedCommit: styled(CommonCommit)`
+    top: -1.5rem;
+    left: -1.5rem;
+  `,
+  BasedCommit: styled(CommonCommit)`
+    top: 1.5rem;
+    left: -1.5rem;
+  `,
+};
+
+export const History = {
+  Container: styled.div`
+    position: relative;
+    display: flex;
+    padding: 1rem 0;
+    margin-left: calc(1.5rem + 2px);
+  `,
+
+  Dot: styled.div<{ main?: boolean; period?: string }>`
+    position: relative;
+    display: flex;
+    flex-shrink: 0;
+    width: ${({ main }) => `${main ? '3rem' : '2.5rem'}`};
+    height: ${({ main }) => `${main ? '3rem' : '2.5rem'}`};
+    margin-right: 0.75rem;
+    background-color: ${({ main, theme }) =>
+      main ? theme.colors.primary.light : theme.colors.success};
+    border-radius: 50%;
+
+    &:before {
+      position: absolute;
+      left: -6rem;
+      z-index: 99;
+      display: block;
+      align-self: center;
+      width: 5rem;
+      text-align: right;
+      content: '${({ period }) => period}';
+    }
+  `,
+  CommitMessage: styled.span<{ main?: boolean }>`
+    align-self: center;
+    justify-items: flex-end;
+    width: 100%;
+    font-size: ${({ main, theme }) => (main ? theme.fontSizes.xxl : theme.fontSizes.l)};
+    font-weight: ${({ main, theme }) => (main ? theme.heads[4].weight : theme.fontWeights.default)};
+  `,
+  Line: styled.div`
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: calc(1.5rem - 4px);
+
+    border: 1px solid ${({ theme }) => theme.colors.success};
+  `,
+};
+
+export const Styled = {
   Container: styled.article`
     display: flex;
     width: 100%;
     height: 100%;
     margin-left: 50%;
   `,
-  Branch: {
-    Container: styled.div<{ draw: boolean; shouldShowHistory: boolean }>`
-      flex-shrink: 0;
-      width: 100%;
-      height: 100%;
-      opacity: 0;
-      transition: opacity 0.3s;
-
-      ${({ draw, shouldShowHistory }) =>
-        draw &&
-        shouldShowHistory &&
-        css`
-          transform-origin: left;
-          animation: scale-up 1s ease-out forwards;
-          animation-delay: 0.25s;
-          @keyframes scale-up {
-            0% {
-              transform: scale(1);
-            }
-            50% {
-              opacity: 1;
-              transform: scale(1.05);
-            }
-            100% {
-              opacity: 1;
-              transform: scale(1);
-            }
-          }
-        `}
-    `,
-    MergedCommitContainer: styled.div`
-      position: relative;
-      display: flex;
-      align-items: flex-start;
-      width: 100%;
-      height: 3rem;
-    `,
-    MergedBranch: styled(Branch)`
-      border-bottom: 0;
-      border-left: 0;
-      border-top-right-radius: 5rem;
-    `,
-
-    BasedBranch: styled(Branch)`
-      border-top: 0;
-      border-left: 0;
-      border-bottom-right-radius: 5rem;
-    `,
-    MergedCommit: styled(Commit)`
-      top: -1.5rem;
-      left: -1.5rem;
-    `,
-    BasedCommit: styled(Commit)`
-      top: 1.5rem;
-      left: -1.5rem;
-    `,
-  },
-  History: {
-    Container: styled.div`
-      position: relative;
-      display: flex;
-      padding: 1rem 0;
-      margin-left: calc(1.5rem + 2px);
-    `,
-
-    Dot: styled.div<{ main?: boolean; period?: string }>`
-      position: relative;
-      display: flex;
-      flex-shrink: 0;
-      width: ${({ main }) => `${main ? '3rem' : '2.5rem'}`};
-      height: ${({ main }) => `${main ? '3rem' : '2.5rem'}`};
-      margin-right: 0.75rem;
-      background-color: ${({ main, theme }) =>
-        main ? theme.colors.primary.light : theme.colors.success};
-      border-radius: 50%;
-
-      &:before {
-        position: absolute;
-        left: -6rem;
-        z-index: 99;
-        display: block;
-        align-self: center;
-        width: 5rem;
-        text-align: right;
-        content: '${({ period }) => period}';
-      }
-    `,
-    CommitMessage: styled.span<{ main?: boolean }>`
-      align-self: center;
-      justify-items: flex-end;
-      width: 100%;
-      font-size: ${({ main, theme }) => (main ? theme.fontSizes.xxl : theme.fontSizes.l)};
-      font-weight: ${({ main, theme }) =>
-        main ? theme.heads[4].weight : theme.fontWeights.default};
-    `,
-    Line: styled.div`
-      position: absolute;
-      top: 0;
-      bottom: 0;
-      left: calc(1.5rem - 4px);
-
-      border: 1px solid ${({ theme }) => theme.colors.success};
-    `,
-  },
+  Branch,
+  History,
 };
